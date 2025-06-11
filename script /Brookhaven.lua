fa6a452ce3d5fd4bac6292f4bcb15564389e10bf
Lucidcreatr <lpprojecttt06@gmail.com>
2025-06-11 20:19:43 +0300

    Update Brookhaven.lua

--- script /Brookhaven.lua
+++ script /Brookhaven.lua
@@ -1,37 +1,34 @@
-loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
+-- Eğer tanımı aşağıdaki gibi bırakacaksan:
+local Maintab = Window:CreateTab("Admin Panel", 4483362458)
 
-Local Window = Rayfield:CreateWindow({
-	Name = "LCX TEAM - ADMİN PANEL"
-	LoadingTitle = "Draconis Studio"
-	LoadingTitle = "Lucid ve Draconis Studio tarafından"
-	ConfigurationSaving = {
-		Enabled = true,
-		FolderName = "AdminPanel"
-		FileName = "AdminGUİ"
-		
-	},
-	Discord ={
-		Enabled = false
-		
-	},
-	KeySystem = false
-
-})
-
--- MAİN TAB --
-Local Maintab = Window:CreateTab("Admin Panel", 4483362458)
-
+-- Slider çağrısını da şu şekilde değiştir:
 Maintab:CreateSlider({
-	Name = "Speed Ayarı",
-	Range = {16, 200},
-	Increment = 1,
-	Suffix = "Speed"
-	CurrentValue = 16,
-	Callback = function(value)
-		local player = game.Players.LocalPlayer
-		Local char = player.Character or player.CharacterAdded:Wait()
-		local humanoid = char:WaitForChild(Humanoid")
-		Humanoid.WalkSpeed = value
-	end
+    Name = "Speed Ayarı",
+    Range = {16, 200},
+    Increment = 1,
+    Suffix = "Speed",
+    CurrentValue = 16,
+    Callback = function(value)
+        local player = game.Players.LocalPlayer
+        local char = player.Character or player.CharacterAdded:Wait()
+        local humanoid = char:WaitForChild("Humanoid")
+        humanoid.WalkSpeed = value
+    end
 })
+-- Eğer tanımı aşağıdaki gibi bırakacaksan:
+local Maintab = Window:CreateTab("Admin Panel", 4483362458)
 
+-- Slider çağrısını da şu şekilde değiştir:
+Maintab:CreateSlider({
+    Name = "Speed Ayarı",
+    Range = {16, 200},
+    Increment = 1,
+    Suffix = "Speed",
+    CurrentValue = 16,
+    Callback = function(value)
+        local player = game.Players.LocalPlayer
+        local char = player.Character or player.CharacterAdded:Wait()
+        local humanoid = char:WaitForChild("Humanoid")
+        humanoid.WalkSpeed = value
+    end
+})
